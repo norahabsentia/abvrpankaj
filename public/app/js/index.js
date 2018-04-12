cc4f6c40dc839c5c8d300fdc444c2203322c8fc3
Norah Absentia <vrushali@absentiavr.com>
2018-04-12 15:27:42 +0530

    Revert "unblock data.json"
    This reverts commit 493daf88d2fea8a57d73cbe270d31f8a62e96728.

--- public/app/js/index.js
+++ public/app/js/index.js
@@ -76,7 +76,8 @@ VideoPlayer.prototype.fetchData = function(uri, callback) {
 VideoPlayer.prototype.init = function() {
     var self = this;
     var video = self.video;
-      $('.js-name').text("HELLO PANKAJ");
+    this.fetchData('data.json', function callback() {
+        $('.js-name').text("HELLO PANKAJ");
         $('.js-month').text("@ Interest Rate of 9.2% p.a.");
 
         $('#animate3 .animate3__line2').append(
@@ -97,8 +98,6 @@ VideoPlayer.prototype.init = function() {
         // retargeting video element
         video = document.getElementsByClassName('vjs-tech')[0];
         CHARLIE.setup(video);
-    this.fetchData('data.json', function callback() {
-      
         return;
     });
 
